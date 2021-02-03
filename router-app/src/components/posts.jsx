45596878c2bdd855b06097557735a4f17ed21314
Xopper <ali.haloua@usmba.ac.ma>
2021-02-03 16:00:39 +0100

    add querySring :)

--- router-app/src/components/posts.jsx
+++ router-app/src/components/posts.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import queryString from "query-string";
 
 /**
  * Self implementaion of isNumber
@@ -8,10 +9,12 @@ import React from "react";
 const isNumber = toCheck =>
 	typeof Number(toCheck) === "number" && !isNaN(Number(toCheck));
 
-const Posts = props => {
-	let { year, month } = props.match.params;
+const Posts = ({ match, location }) => {
+	let { year, month } = match.params;
 	month = isNumber(month) ? Number(month) : "";
 	year = isNumber(year) ? Number(year) : "";
+	const query = queryString.parse(location.search);
+	console.log(query);
 	return (
 		<div>
 			<h1>Posts</h1>
