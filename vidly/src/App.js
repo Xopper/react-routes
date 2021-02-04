4d7cc9643dd7f282e64d0fc28e3e3d77d84c4038
Xopper <ali.haloua@usmba.ac.ma>
2021-02-04 10:00:54 +0100

    pushing route exercise

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,15 +1,37 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Movies from "./components/movies";
 import "./App.css";
+import NavBar from "./components/navBar";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect
+} from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import notFount from "./components/notFound";
 
 class App extends Component {
-  render() {
-    return (
-      <main className="container">
-        <Movies />
-      </main>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<Fragment>
+					<NavBar />
+					<main className="container mt-4">
+						<Switch>
+							<Route path="/customers" component={Customers} />
+							<Route path="/rentals" component={Rentals} />
+							<Route path="/not-found" component={notFount} />
+							<Route path="/movies" component={Movies} />
+							<Redirect exact from="/" to="/movies" />
+							<Redirect to="/not-found" />
+						</Switch>
+					</main>
+				</Fragment>
+			</Router>
+		);
+	}
 }
 
 export default App;
---
+++ vidly/src/components/notFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const notFount = () => {
+	return <h1>Not Fount</h1>;
+};
+
+export default notFount;
